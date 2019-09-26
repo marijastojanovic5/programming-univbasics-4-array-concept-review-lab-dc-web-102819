2724a3dc7ff995554e1c2ce449ee3ebe514c05e2
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-09-26 00:56:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,19 @@
 def find_element_index(array, value_to_find)
-
+ 
+ counter=0
+  while counter<array.length do
+    
+    if array[counter]=== value_to_find
+      return counter
+		end
+    counter+=1 
+  end
 end
 
 def find_max_value(array)
- array.max
- puts [max]
+
 end
 
 def find_min_value(array)
- array.min
- puts [min]
+
 end
