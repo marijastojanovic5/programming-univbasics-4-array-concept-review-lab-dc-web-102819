d6f07a89e79da9aaee9edbbb94b1e623203bc5d9
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-09-26 01:16:51 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -22,5 +22,12 @@ def find_max_value(array)
  end
 
 def find_min_value(array)
-
+ counter=0
+  while counter<array.length do
+    
+    if array[counter]=== array.min
+      return counter
+		end
+    counter+=1 
+  end
 end
